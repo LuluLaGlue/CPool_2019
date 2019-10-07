f5feaf4a6e267e211d0f2d41e9c98411d8effbe8
LuluLaGlue <56148103+LuluLaGlue@users.noreply.github.com>
2019-10-07 14:43:33 +0200

    Add files via upload

--- CPool_Day06_2019/my_strncmp.c
+++ CPool_Day06_2019/my_strncmp.c
@@ -10,12 +10,10 @@ int my_strncmp(char *s1, char *s2, int n)
     int i;
 
     i = 0;
-    while (s1[i] == s2[i] && s1[i] && s2[i] && i < n){
+    while (s1[i] == s2[i] && s1[i] && s2[i] && i < n)
         i++;
-    }
-    if ((s1[i] == '\0' && s2[i] == '\0') || i == n){
+    if ((s1[i] == '\0' && s2[i] == '\0') || i == n)
         return (0);
-    }
     else if (s1[i] > s2[i]){
         return (1);
     }
