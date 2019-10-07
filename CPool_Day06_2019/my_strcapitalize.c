596f51454fd77e5e68d377f605add6427a42d3b8
LuluLaGlue <56148103+LuluLaGlue@users.noreply.github.com>
2019-10-07 17:42:17 +0200

    Add files via upload

--- CPool_Day06_2019/my_strcapitalize.c
+++ CPool_Day06_2019/my_strcapitalize.c
@@ -14,7 +14,9 @@ char *my_strcapitalize(char *str)
     while (str[i]){
         if (str[i] >= 'A' && str[i] <= 'Z' && str[i - 1] != ' ')
             str[i] = str[i] + 32;
-        if (str[i] >= 'a' && str[i] <= 'z' && str[i - 1] == ' ')
+        else if (str[i] >= 'a' && str[i] <= 'z' && (str[i - 1] == ' ' ||
+        (str[i - 1] >= 33 && str[i - 1] <= 47) ||
+        (str[i - 1] >= 58 && str[i - 1] <= 64)))
             str[i] = str[i] - 32;
         i++;
     }
--- CPool_Day06_2019/tests/test_my_strcapitalize.c
+++ CPool_Day06_2019/tests/test_my_strcapitalize.c
@@ -13,9 +13,12 @@ Test(my_strcapitalize, capitalize_first_letter)
 {
     char str[14] = "bla 42bla bLa";
     char s2[4] = "Bla";
+    char s3[47] = "hey, how are you? 42WORds forty-two: fifty+one";
 
     my_strcapitalize(str);
     my_strcapitalize(s2);
+    my_strcapitalize(s3);
     cr_assert_str_eq(str, "Bla 42bla Bla");
     cr_assert_str_eq(s2, "Bla");
+    cr_assert_str_eq(s3, "Hey, How Are You? 42words Forty-Two: Fifty+One");
 }
