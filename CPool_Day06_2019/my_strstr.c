3872503e0d72d76222ab3f1728a41e382dcd96da
LuluLaGlue <56148103+LuluLaGlue@users.noreply.github.com>
2019-10-07 18:37:16 +0200

    updated my_strstr

--- CPool_Day06_2019/my_strstr.c
+++ CPool_Day06_2019/my_strstr.c
@@ -5,25 +5,28 @@
 ** my_strstr
 */
 
-char *my_strstr(char *str, char const *to_find)
-{
-    int i, j , begin;
+int my_strlen(char *str);
 
-    i = 0;
-    j = 0;
-    begin = 0;
-    while (str[i] && to_find[j]){
-        if (str[i] == to_find[j] && j == 0){
-            begin = i;
+int my_strncmp(char *s1, char *s2, int n);
+
+char *my_strchr(const char *s, int c)
+{
+    do {
+        if (*s == c){
+            return (char *)s;
         }
-        if (str[i] == to_find[j])
-            j++;
-        else
-            j = 0;
-        i++;
+    } while (*s++);
+    return (0);
+}
+
+char *my_strstr(char *str, const char *to_find)
+{
+    char *p = str;
+    int len = my_strlen(to_find);
+
+    for (; (p = my_strchr(p, *to_find)) != 0; p++){
+        if (my_strncmp(p, to_find, len) == 0)
+            return (char *)p;
     }
-    if (to_find[j] == '\0')
-        return (&str[begin]);
-    else
-        return (0);
+    return (0);
 }
