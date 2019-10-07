6d7aa743c3d13e17b7386713ea25cf12e2464eec
LuluLaGlue <56148103+LuluLaGlue@users.noreply.github.com>
2019-10-07 18:47:03 +0200

    Match / nMatch
    find out if two strings match / count the number of times two strings match

--- CPool_match-nmatch_2019/nmatch.c
+++ CPool_match-nmatch_2019/nmatch.c
@@ -5,7 +5,23 @@
 ** nmatch
 */
 
+int match(char const *s1, char const *s2);
+
 int nmatch(char const *s1, char const *s2)
 {
+    int count;
+    int i;
+    char check_match;
 
+    check_match = 0;
+    count = 0;
+    i = 0;
+    while (s1[i] != '\0'){
+        if (match(s1 + i++, s2) != check_match){
+            check_match = !check_match;
+            if (check_match == 1)
+                ++count;
+        }
+    }
+    return (count);
 }
